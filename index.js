c422eef4758ad313b1073b89c6a16ffa933f5445
Jason Presher <victor.presher@7-11.com>
2020-05-26 17:01:24 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let palindrome = (phrase) => {
+    if(typeof phrase === 'string'){
+        let reversePhrase = phrase.split("").reverse().join("");
+        let comparedPhrase = phrase.localeCompare(reversePhrase);
+        if (comparedPhrase === 0){
+            console.log('The Phrase ' + phrase + ' is a palindrome')
+        }else{
+            console.log('The phrase ' + phrase + ' is not a palindrome');
+        }
+    }
+    if(typeof phrase === 'number'){
+        let reversePhrase = phrase.toString()
+        .split('')
+        .reverse()
+        .join('');
+         let remain = parseInt(reversePhrase) - phrase;
+         if(remain === 0 && Math.sign(remain) === 0){
+             console.log('The number ' + phrase + ' is a palindrome');
+         }else{
+             console.log('The number ' + phrase + ' is not a palindrome');
+         }
+    }
+}
+palindrome(909);
+module.exports = palindrome;
