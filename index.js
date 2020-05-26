ddb947d9484a2bbc72ea2728ce1590191c62d5ef
Jason Presher <victor.presher@7-11.com>
2020-05-26 17:16:41 -0500

    Update the nmber filter

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ let palindrome = (phrase) => {
         .reverse()
         .join('');
          let remain = parseInt(reversePhrase) - phrase;
-         if(remain === 0 && Math.sign(remain) === 0){
+         if(remain === 0 && Math.sign(remain) === 0 && Number.isInteger(phrase)){
              console.log('The number ' + phrase + ' is a palindrome');
          }else{
              console.log('The number ' + phrase + ' is not a palindrome');
          }
     }
 }
-palindrome(909);
+palindrome(); //pass in a number or a word
 module.exports = palindrome;
